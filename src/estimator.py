29dfd11b7a9499fbcb018a035d5c51dbed733c15
ronjones340 <ej07696@gmail.com>
2020-04-12 16:19:16 +0300

    *ChangesAgain*

--- src/estimator.py
+++ src/estimator.py
@@ -1,14 +1,30 @@
 from periodtype import (days,weeks,months)
 
+data = {
+    "region": {
+    "name": "Africa",
+    "avgAge": 19.7,
+    "avgDailyIncomeInUSD": 4,
+    "avgDailyIncomePopulation": 0.73
+    },
+    "periodType": "days",
+    "timeToElapse": 38,
+    "reportedCases": 2747,
+    "population": 92931687,
+    "totalHospitalBeds": 678874
+
+    }
 def estimator(data):
     ptype = data["periodType"]
-    if (str(ptype) == 'Days'):
+    if (str(ptype) == 'days'):
         x = days(data)
-    elif (str(ptype) == 'Weeks'):
+        return x
+    elif (str(ptype) == 'weeks'):
         x = weeks(data)
+        return x
     else:
         x = months(data)
-    return x
+        return x
 
 
 #estimator(data)
